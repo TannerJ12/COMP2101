149f3f47176fc1de6354aa22717babe100595190
TannerJ12 <90281115+TannerJ12@users.noreply.github.com>
2021-12-13 19:40:33 -0500

    Add files via upload

---
+++ Lab5Script.ps1
@@ -0,0 +1,120 @@
+﻿ function get-sys {
+
+Param(
+   [Parameter(Position=1)]
+   [switch]$drives ,
+  
+   [Parameter(Position=2)]
+   [switch]$network ,
+ 
+   [Parameter(Position=3)]
+   [switch]$system
+   ) 
+
+if ($system) {
+Get-CimInstance win32_computersystem |
+    foreach {
+        New-Object -TypeName psobject -Property @{
+            "System Hardware Description" = $_.Description
+        } 
+        } |
+        Format-List "System Hardware Description"
+
+Get-CimInstance win32_operatingsystem | 
+    foreach { 
+        New-Object -TypeName psobject -Property @{
+            "System Name" = $_.Name
+            "Version Number" = $_.Version
+        }
+        }|
+        Format-List "System Name" ,
+                    "Version Number"
+
+Get-CimInstance win32_processor |
+    foreach { 
+        New-Object -TypeName psobject -Property @{
+            "Speed" = $_.MaxClockSpeed
+            "Number Of Cores" = $_.NumberOfCores
+            "Size of L1" = $_.L1Cachesize
+            "Size of L2" = $_.L2CacheSize
+            "Size of L3" = $_.L3CacheSize
+            }
+            }|
+            Format-List "Speed" , 
+                        "Number of Cores" ,
+                        "Size of L1" ,
+                        "Size of L2" ,
+                        "Size of L3"
+Get-CimInstance win32_physicalmemory |
+    foreach {
+        New-Object -TypeName psobject -Property @{
+            "Vendor" = $_.SerialNumber
+            "Description" = $_.Description
+            "Size" = $_.Capacity
+            "Bank Slot" = $_.BankLabel
+            "Capacity" = $_.Capacity
+        }
+        }|
+        Format-List "Vendor" ,
+                    "Description" ,
+                    "Size" ,
+                    "Bank Slot" ,
+                    "Capacity"
+
+Get-CimInstance win32_videocontroller |
+    foreach {
+        New-Object -TypeName psobject -Property @{
+            Description = $_.Description
+            "Vendor" = $_.DeviceID
+            "Horizontal Resolution" = $_.CurrentHorizontalResolution
+            "Vertical Resolution" = $_.CurrentVerticalResolution
+        }
+        $HoriReso = $_.CurrentHorizontalResolution
+        $VertiReso = $_.CurrentVerticalResolution
+        }|
+        Format-List Description ,
+                    "Vendor" ,
+                    "Horizontal Resolution" ,
+                    "Vertical Resolution"
+"Current Resolution is $HoriReso x $VertiReso " 
+}          
+
+
+if ($drives) {
+ $diskdrives = Get-CIMInstance CIM_diskdrive
+
+  foreach ($disk in $diskdrives) {
+      $partitions = $disk|get-cimassociatedinstance -resultclassname CIM_diskpartition
+      foreach ($partition in $partitions) {
+            $logicaldisks = $partition | get-cimassociatedinstance -resultclassname CIM_logicaldisk
+            foreach ($logicaldisk in $logicaldisks) {
+                     new-object -typename psobject -property @{Manufacturer=$disk.Manufacturer
+                                                               Location=$partition.deviceid
+                                                               Drive=$logicaldisk.deviceid
+                                                               "Size(GB)"=$logicaldisk.size / 1gb -as [int]
+                                                               }
+           }
+    }
+  }
+  }
+  
+  if ($network) {
+  Get-CimInstance -class win32_networkadapterconfiguration | where-object ipenabled |
+    foreach {
+        New-Object -TypeName psobject -Property @{
+            "Description" = $_.Description
+            "Index" = $_.index
+            "IP Address" = $_.ipaddress
+            "Subnet" = $_.ipsubnet
+            "DNS Domain" = $_.dnsdomain
+            "DNS Server" = $_.dnsserversearchorder
+            }
+            }|
+            Format-List "Description" ,
+                        "Index" ,
+                        "IP Address" ,
+                        "Subnet" ,
+                        "DNS Domain" ,
+                        "DNS Server"
+}
+}
